bbbb3f6d671077a402e4476b32a1044ab1fe67c9
Benjamin Skinstad <benjas@netcompany.com>
2022-09-29 15:11:22 +0200

    added abilties

--- Projects/Games/vampire lol game/Assets/Scripts/PlayerController.cs
+++ Projects/Games/vampire lol game/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -47,16 +48,81 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            
+
             Debug.Log("testing purposes");
             if (this.GetComponent<CustomTest>())
             {
                 Debug.Log("it alread have custom test ");
-                this.gameObject.GetComponent<CustomTest>().IncreaseLevel();
+
+                
+
+                this.gameObject.GetComponent<PlayerAbilityAbosulteZero>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityAceInTheHole>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityArcanopulse>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityBalefulStrike>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityBarrier>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityBloomingBurst>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityBoomerangThrow>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityBurnOut>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityCellDivision>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityChainOfCorruption>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityCleanse>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityDarkMatter>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityDisintigrate>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityEventHorizon>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityFlameChompers>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityGhost>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityMakeItRain>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityMoltenShield>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityPersonalSpace>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityRewind>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityShockwave>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityTheCulling>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityTravlersCall>().IncreaseLevel();
+                this.gameObject.GetComponent<PlayerAbilityZap>().IncreaseLevel();
+
 
             }
             else
             {
-                this.gameObject.AddComponent<CustomTest>();
+                DirectoryInfo dir = new DirectoryInfo(@"C:\Users\benjas\Documents\git\benski\portfolio\Projects\Games\vampire lol game\Assets\Scripts\playerbuffs");
+                FileInfo[] info = dir.GetFiles("*.cs");
+                foreach (FileInfo f in info)
+                {
+                    string ability = Path.GetFileName(f.ToString());
+                    ability = ability.Substring(0, ability.Length - 3);
+                    Debug.Log(ability);
+                    this.gameObject.AddComponent(System.Type.GetType(ability));
+
+                }
+
+
+                //this.gameObject.AddComponent<PlayerAbilityAbosulteZero>();
+                //this.gameObject.AddComponent<PlayerAbilityAceInTheHole>();
+                //this.gameObject.AddComponent<PlayerAbilityArcanopulse>();
+                //this.gameObject.AddComponent<PlayerAbilityBalefulStrike>();
+                //this.gameObject.AddComponent<PlayerAbilityBarrier>();
+                //this.gameObject.AddComponent<PlayerAbilityBloomingBurst>();
+                //this.gameObject.AddComponent<PlayerAbilityBoomerangThrow>();
+                //this.gameObject.AddComponent<PlayerAbilityBurnOut>();
+                //this.gameObject.AddComponent<PlayerAbilityCellDivision>();
+                //this.gameObject.AddComponent<PlayerAbilityChainOfCorruption>();
+                //this.gameObject.AddComponent<PlayerAbilityCleanse>();
+                //this.gameObject.AddComponent<PlayerAbilityDarkMatter>();
+                //this.gameObject.AddComponent<PlayerAbilityDisintigrate>();
+                //this.gameObject.AddComponent<PlayerAbilityEventHorizon>();
+                //this.gameObject.AddComponent<PlayerAbilityFlameChompers>();
+                //this.gameObject.AddComponent<PlayerAbilityGhost>();
+                //this.gameObject.AddComponent<PlayerAbilityMakeItRain>();
+                //this.gameObject.AddComponent<PlayerAbilityMoltenShield>();
+                //this.gameObject.AddComponent<PlayerAbilityPersonalSpace>();
+                //this.gameObject.AddComponent<PlayerAbilityRewind>();
+                //this.gameObject.AddComponent<PlayerAbilityShockwave>();
+                //this.gameObject.AddComponent<PlayerAbilityTheCulling>();
+                //this.gameObject.AddComponent<PlayerAbilityTravlersCall>();
+                //this.gameObject.AddComponent<PlayerAbilityZap>();
+
             }
         }
     }
