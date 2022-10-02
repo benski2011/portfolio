79885272d7d75c3d6770e60ee3a944f52fe4605a
ben ski <benski@live.no>
2022-10-02 21:50:44 +0200

    added levels

--- Projects/Games/bullethell/Assets/Scripts/enemyScript.cs
+++ Projects/Games/bullethell/Assets/Scripts/enemyScript.cs
@@ -40,8 +40,8 @@ public class enemyScript : MonoBehaviour
 
         if (Time.time > canfire)
         {
-            //Shoot();
-            ShootRay();
+            Shoot();
+            //ShootRay();
             //Shotgun();
             //ArcShot();
             //DelayedShoot();
