617e502d12a8c6f0834991eec7da8fbd954cd99d
ben ski <benski@live.no>
2022-10-02 23:46:48 +0200

    made it possible to select levels

--- Projects/Games/bullethell/Assets/Scripts/mapEnabler.cs
+++ Projects/Games/bullethell/Assets/Scripts/mapEnabler.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 /// </summary>
 public static class mapEnabler 
 {
-    public static bool[] map = new[] { true, false, false, false, false, false, false, false, false, false, false };
+    public static bool[] map = new[] { true, true, true, true, true, true, true, true, true, true, true };
+
+    //public static bool[] map = new[] { true, false, false, false, false, false, false, false, false, false, false };
 
 }
