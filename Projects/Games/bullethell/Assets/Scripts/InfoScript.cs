ad3551fe3a87e73134d8cd19b585fb8bdf3bb685
Benjamin Skinstad <benjas@netcompany.com>
2022-10-05 20:50:03 +0200

    added more level, made them lore accurate

---
+++ Projects/Games/bullethell/Assets/Scripts/InfoScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [TextArea]
+    public string Info;
+}
---
+++ Projects/Games/bullethell/Assets/Scripts/enemy script/EnemyBaseScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBaseScript : MonoBehaviour
+{
+
+    public int EnemyHp;
+    public int EnemyShieldHp;
+    public float EnemyMoveSpeed;
+
+    public float EnemyFireRate;
+
+    public void EnemyDeath()
+    {
+
+    }
+
+    
+}
